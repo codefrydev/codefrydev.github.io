b0818877235889afd2fe48beb9d27181f4401758
PrashantUnity <priyadarshisadiku@gmail.com>
2026-05-20 01:19:18 +0530

    remove external icons

--- static/js/search-palette.js
+++ static/js/search-palette.js
@@ -215,7 +215,7 @@
         '">' +
         item.category +
         '</span></span></span>' +
-        '<i class="ph-bold ph-arrow-right search-palette__result-arrow" aria-hidden="true"></i>';
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="14" height="14" class="search-palette__result-arrow" aria-hidden="true"><polyline points="9 18 15 12 9 6"/></svg>';
       li.appendChild(a);
       resultsList.appendChild(li);
     });
